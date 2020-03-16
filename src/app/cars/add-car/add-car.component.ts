4fa0366204641b46ae00b851d6ed770c3549a619
Grzegorz Ratajczak <Grzegorz.Ratajczak@asseco.pl>
2020-03-16 23:09:15 +0100

    add factories list to add car component

--- src/app/cars/add-car/add-car.component.ts
+++ src/app/cars/add-car/add-car.component.ts
@@ -16,7 +16,11 @@ export class AddCarComponent implements OnInit {
   groundClearances: string[] = ['Low', 'Medium', 'High'];
   fuels: string[] = ['Diesel', 'Electro', 'Hydrogen', 'Hybrid', 'Petrol', 'Unknown'];
   countries: string[] = ['AT', 'AU', 'DE', 'FR', 'GB', 'IT', 'JP', 'SE', 'US'];
-
+  factories: string[] = ['Acura', 'Alfa Romeo', 'Audi', 'Ariel', 'Aston Martin', 'Austin', 'Bentley', 'BMW', 'Bugatti', 'Buick',
+    'Cadillac', 'Caterham', 'Chevrolet', 'Chrysler', 'Citroen', 'Dodge', 'DS', 'Fiat', 'Ford', 'GMC', 'Honda', 'Hummer', 'Infiniti',
+    'Jaguar', 'KTM', 'Lamborghini', 'Lancia', 'Land Rover', 'Lotus', 'Maserati', 'Mazda', 'McLaren', 'Mercedes-Benz', 'MG', 'Mini',
+    'Mitsubishi', 'Nissan', 'Pagani', 'Peugeot', 'Plymouth', 'Pontiac', 'Porshe', 'Ram', 'Renault', 'Rover', 'RUF', 'Scuderia Cameron',
+    'Smart', 'Subaru', 'Suzuki', 'TVR', 'Vauxhall/Opel', 'Volvo', 'Volkswagen'];
 
   constructor(private carService: CarService, private router: Router) {
   }
