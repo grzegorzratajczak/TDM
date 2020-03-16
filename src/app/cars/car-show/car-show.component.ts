263c9e24616838a13c6abb0c4c57192da58f3a6f
Grzegorz Ratajczak <Grzegorz.Ratajczak@asseco.pl>
2020-03-16 17:20:40 +0100

    give color to car cost attr

--- src/app/cars/car-show/car-show.component.ts
+++ src/app/cars/car-show/car-show.component.ts
@@ -16,4 +16,27 @@ export class CarShowComponent implements OnInit {
   ngOnInit() {
   }
 
+  getCategoryColorByCarCost(carCost: number) {
+    if (carCost < 7) {
+      return 'gray';
+    }
+    if (carCost < 11) {
+      return 'green';
+    }
+    if (carCost < 15) {
+      return 'blue';
+    }
+    if (carCost < 19) {
+      return 'yellow';
+    }
+    if (carCost < 23) {
+      return 'red';
+    }
+    if (carCost < 27) {
+      return 'purple';
+    } else {
+      return 'gold';
+    }
+  }
+
 }
